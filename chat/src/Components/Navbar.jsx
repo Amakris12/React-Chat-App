1f9c43fe247919ccca20282a58e3524bdce58861
Amakris12 <amakri386@west-mec.org>
2022-03-24 14:54:45 -0700

    Started chat app

---
+++ chat/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>Header Section</h2>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+        </nav>
+    )
+}
+
+export default Navbar
